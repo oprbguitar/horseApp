7e9059a4bb280a62c3780b8cde3c5cca48db7ef5
oprbguitar <oprbguitar@gmail.com>
2013-06-10 21:28:30 -0500

    creaccion del proyecto
    tabla nombre de caballo

---
+++ raceApp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RaceApp::Application.config.secret_token = '884520b915343e5334bf9df0fcde3a6a75da3b072a2b50b39a0cce623a7141532ffcb97fd0ef9a381d4d560b868d089c3c3f903ab07318d0d20dc8a25dad61e6'
